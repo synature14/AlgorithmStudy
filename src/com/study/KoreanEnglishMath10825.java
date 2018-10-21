2193b63009ffed9c239dc317db112750e2379031
suyeon <synature14@gmail.com>
2018-10-21 16:02:30 +0900

    #10825 정답 맞춤

--- src/com/study/KoreanEnglishMath10825.java
+++ src/com/study/KoreanEnglishMath10825.java
@@ -2,6 +2,8 @@ package com.study;
 // 10825 국영수
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 
@@ -19,7 +21,6 @@ public class KoreanEnglishMath10825 {
             this.mathScore = mathScore;
         }
     }
-
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -58,66 +59,97 @@ public class KoreanEnglishMath10825 {
             totalArrayList.add(eachScore);
         }
 
-        for(int i=0; i<totalArrayList.size()-1; i++) {
-            for(int j=i+1; j<totalArrayList.size(); j++) {
-                Score score1 = totalArrayList.get(i);
-                Score score2 = totalArrayList.get(j);
-
-                if (score1.koreanScore < score2.koreanScore) {        // 1. 국어 점수 비교 (내림차순)
-                    Score temp = score1;
-                    totalArrayList.set(i, score2);
-                    totalArrayList.set(j, temp);
+        // Comparable 커스텀
+        Collections.sort(totalArrayList, new Comparator<Score>() {
+            @Override
+            public int compare(Score o1, Score o2) {
+                int ret = 1;
+                if (o1.koreanScore > o2.koreanScore) {        // 1. 국어 점수 비교 (내림차순)
+                    ret = -1;        // (앞의 값 - 뒤의 값 > 0 --> 내림차순)
+                    // 따라서 음수 리턴 : 순서 그대로 놔두고 싶을때
                 }
-            }
-        } // for()
+                    // 2. 영어 점수 비교 (오름차순)
+                if (o1.koreanScore == o2.koreanScore) {
+                        if (o1.englishScore < o2.englishScore) {
+                            ret = -1;
 
-        for(int i=0; i<totalArrayList.size()-1; i++) {
-            for(int j=i+1; j<totalArrayList.size(); j++) {
-                Score score1 = totalArrayList.get(i);
-                Score score2 = totalArrayList.get(j);
+                        } else if (o1.englishScore == o2.englishScore) {
+                            if (o1.mathScore > o2.mathScore) {     // 3. 수학 점수 비교 (내림차)
+                                ret = -1;
 
-                // 2. 영어 점수 비교 (오름차)
-                if (score1.koreanScore == score2.koreanScore
-                        && score1.englishScore > score2.englishScore) {
-                    Score temp = score2;
-                    totalArrayList.set(j, score1);
-                    totalArrayList.set(i, temp);
+                            } else if(o1.mathScore == o2.mathScore) {      // 4. 모든 점수가 같으면, 이름이 사전순으로 오름차순
+                                if(o1.name.compareTo(o2.name) < 0) {    // o1의 이름의 사전적 순서가 o2에 비해 작다 --> o1이 o2보다 더 앞에 배치
+                                    ret = -1;
+                                }
+                            }
+                        }
                 }
+
+                return ret;
             }
-        }
+        });
 
-        for(int i=0; i<totalArrayList.size()-1; i++) {
-            for(int j=i+1; j<totalArrayList.size(); j++) {
-                Score score1 = totalArrayList.get(i);
-                Score score2 = totalArrayList.get(j);
 
-                if (score1.koreanScore == score2.koreanScore && score1.englishScore == score2.englishScore
-                        && score1.mathScore != score2.mathScore) {
-                    if (score1.mathScore < score2.mathScore) {     // 3. 수학 점수 비교 (내림차)
-                        Score temp = score1;
-                        totalArrayList.set(i, score2);
-                        totalArrayList.set(j, temp);
-                    }
-                }
-            }
-        }
-
-        for(int i=0; i<totalArrayList.size()-1; i++) {
-            for (int j=i+1; j<totalArrayList.size(); j++) {
-                Score score1 = totalArrayList.get(i);
-                Score score2 = totalArrayList.get(j);
-
-                // 4. 모든 점수가 같으면, 이름이 사전순으로 오름차순
-                if (score1.koreanScore == score2.koreanScore && score1.englishScore == score2.englishScore
-                        && score1.mathScore == score2.mathScore) {
-                    if (score1.name.compareTo(score2.name) > 0) { // score1.name > score2.name --> score2.name이 사전순으로 앞이다
-                        Score temp = score2;
-                        totalArrayList.set(j, score1);
-                        totalArrayList.set(i, temp);
-                    }
-                }
-            }
-        }
+//        for(int i=0; i<totalArrayList.size()-1; i++) {
+//            for(int j=i+1; j<totalArrayList.size(); j++) {
+//                Score score1 = totalArrayList.get(i);
+//                Score score2 = totalArrayList.get(j);
+//
+//                if (score1.koreanScore < score2.koreanScore) {        // 1. 국어 점수 비교 (내림차순)
+//                    Score temp = score1;
+//                    totalArrayList.set(i, score2);
+//                    totalArrayList.set(j, temp);
+//                }
+//            }
+//        } // for()
+//
+//        for(int i=0; i<totalArrayList.size()-1; i++) {
+//            for(int j=i+1; j<totalArrayList.size(); j++) {
+//                Score score1 = totalArrayList.get(i);
+//                Score score2 = totalArrayList.get(j);
+//
+//                // 2. 영어 점수 비교 (오름차)
+//                if (score1.koreanScore == score2.koreanScore
+//                        && score1.englishScore > score2.englishScore) {
+//                    Score temp = score2;
+//                    totalArrayList.set(j, score1);
+//                    totalArrayList.set(i, temp);
+//                }
+//            }
+//        }
+//
+//        for(int i=0; i<totalArrayList.size()-1; i++) {
+//            for(int j=i+1; j<totalArrayList.size(); j++) {
+//                Score score1 = totalArrayList.get(i);
+//                Score score2 = totalArrayList.get(j);
+//
+//                if (score1.koreanScore == score2.koreanScore && score1.englishScore == score2.englishScore
+//                        && score1.mathScore != score2.mathScore) {
+//                    if (score1.mathScore < score2.mathScore) {     // 3. 수학 점수 비교 (내림차)
+//                        Score temp = score1;
+//                        totalArrayList.set(i, score2);
+//                        totalArrayList.set(j, temp);
+//                    }
+//                }
+//            }
+//        }
+//
+//        for(int i=0; i<totalArrayList.size()-1; i++) {
+//            for (int j=i+1; j<totalArrayList.size(); j++) {
+//                Score score1 = totalArrayList.get(i);
+//                Score score2 = totalArrayList.get(j);
+//
+//                // 4. 모든 점수가 같으면, 이름이 사전순으로 오름차순
+//                if (score1.koreanScore == score2.koreanScore && score1.englishScore == score2.englishScore
+//                        && score1.mathScore == score2.mathScore) {
+//                    if (score1.name.compareTo(score2.name) > 0) { // score1.name > score2.name --> score2.name이 사전순으로 앞이다
+//                        Score temp = score2;
+//                        totalArrayList.set(j, score1);
+//                        totalArrayList.set(i, temp);
+//                    }
+//                }
+//            }
+//        }
 
         for(int i=0; i<num; i++) {
             System.out.println(totalArrayList.get(i).name);
