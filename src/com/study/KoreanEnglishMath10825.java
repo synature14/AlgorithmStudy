eeb535c03338b82b152b749f82592fd27dfa5b31
suyeon <synature14@gmail.com>
2018-10-21 15:14:58 +0900

    #10825 시간초과

--- src/com/study/KoreanEnglishMath10825.java
+++ src/com/study/KoreanEnglishMath10825.java
@@ -2,8 +2,6 @@ package com.study;
 // 10825 국영수
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Scanner;
 
 
@@ -22,29 +20,6 @@ public class KoreanEnglishMath10825 {
         }
     }
 
-    static class ScoreComparator implements Comparator<Score> {
-        @Override
-        public int compare(Score score1, Score score2) {
-            int ret = 0;
-            if (score1.koreanScore > score2.koreanScore) {        // 1. 국어 점수 비교 (내림차순)
-                ret = 1;
-            }
-
-            if (score1.koreanScore == score2.koreanScore) {
-                if (score1.englishScore < score2.englishScore) {   // 2. 영어 점수 비교 (오름차)
-                    ret = 1;
-                }
-
-                if (score1.englishScore == score2.englishScore) {
-                    if (score1.mathScore > score2.mathScore) {     // 3. 수학 점수 비교 (내림차)
-                        ret = 1;
-                    }
-                }
-            }
-            return ret;
-
-        }
-    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -58,29 +33,95 @@ public class KoreanEnglishMath10825 {
             String infoString = scan.nextLine();
             String[] info = infoString.split(" ");
 
-            /* --- error ---
+            String name = "";
+            int koreanScore = 0;
+            int englishScore = 0;
+            int mathScore = 0;
+
             for(int index = 0; index<4; index++) {
-                if (index == 0) {
-                    String name = info[0];
-                    System.out.println("name : " + name);
-                } else {
-                      int score = Integer.parseInt(info[i]);
-                      System.out.println("info : " + score);
+                switch (index) {
+                    case 0:
+                        name = info[index];
+                        break;
+                    case 1:
+                        koreanScore = Integer.parseInt(info[index]);
+                        break;
+                    case 2:
+                        englishScore = Integer.parseInt(info[index]);
+                        break;
+                    case 3:
+                        mathScore = Integer.parseInt(info[index]);
+                        break;
                 }
             }
-            */
-            String name = info[0];
-            int koreanScore = Integer.parseInt(info[1]);
-            int englishScore = Integer.parseInt(info[2]);
-            int mathScore = Integer.parseInt(info[3]);
-            System.out.println("info : " + koreanScore + ", " + englishScore + ", " + mathScore);
-
             Score eachScore = new Score(name, koreanScore, englishScore, mathScore);
             totalArrayList.add(eachScore);
         }
 
-        Collections.sort(totalArrayList, ScoreComparator);
+        for(int i=0; i<totalArrayList.size()-1; i++) {
+            for(int j=i+1; j<totalArrayList.size(); j++) {
+                Score score1 = totalArrayList.get(i);
+                Score score2 = totalArrayList.get(j);
 
+                if (score1.koreanScore < score2.koreanScore) {        // 1. 국어 점수 비교 (내림차순)
+                    Score temp = score1;
+                    totalArrayList.set(i, score2);
+                    totalArrayList.set(j, temp);
+                }
+            }
+        } // for()
+
+        for(int i=0; i<totalArrayList.size()-1; i++) {
+            for(int j=i+1; j<totalArrayList.size(); j++) {
+                Score score1 = totalArrayList.get(i);
+                Score score2 = totalArrayList.get(j);
+
+                // 2. 영어 점수 비교 (오름차)
+                if (score1.koreanScore == score2.koreanScore
+                        && score1.englishScore > score2.englishScore) {
+                    Score temp = score2;
+                    totalArrayList.set(j, score1);
+                    totalArrayList.set(i, temp);
+                }
+            }
+        }
+
+        for(int i=0; i<totalArrayList.size()-1; i++) {
+            for(int j=i+1; j<totalArrayList.size(); j++) {
+                Score score1 = totalArrayList.get(i);
+                Score score2 = totalArrayList.get(j);
+
+                if (score1.koreanScore == score2.koreanScore && score1.englishScore == score2.englishScore
+                        && score1.mathScore != score2.mathScore) {
+                    if (score1.mathScore < score2.mathScore) {     // 3. 수학 점수 비교 (내림차)
+                        Score temp = score1;
+                        totalArrayList.set(i, score2);
+                        totalArrayList.set(j, temp);
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<totalArrayList.size()-1; i++) {
+            for (int j=i+1; j<totalArrayList.size(); j++) {
+                Score score1 = totalArrayList.get(i);
+                Score score2 = totalArrayList.get(j);
+
+                // 4. 모든 점수가 같으면, 이름이 사전순으로 오름차순
+                if (score1.koreanScore == score2.koreanScore && score1.englishScore == score2.englishScore
+                        && score1.mathScore == score2.mathScore) {
+                    if (score1.name.compareTo(score2.name) > 0) { // score1.name > score2.name --> score2.name이 사전순으로 앞이다
+                        Score temp = score2;
+                        totalArrayList.set(j, score1);
+                        totalArrayList.set(i, temp);
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<num; i++) {
+            System.out.println(totalArrayList.get(i).name);
+        }
     }
 
 }
