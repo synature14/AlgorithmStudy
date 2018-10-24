d5b2482b2d42ff8d73355b9125974c7e2393d68a
suyeon <synature14@gmail.com>
2018-10-24 19:46:08 +0900

    #1068 테스트케이스 충족

--- src/com/study181030/Tree1068.java
+++ src/com/study181030/Tree1068.java
@@ -1,69 +1,63 @@
 package com.study181030;
 
 // 뉴스전하기 문제와 비슷
-// 1차 실패 : root가 무조건 0번째 노드라고 생각하고 풀었음
+// 1차 시도 : 실패, root가 무조건 0번째 노드라고 생각하고 풀었음
 // 2차 시도 : 입력값이 -1일때 root인덱스로 설정, searchLeafNode(root) 호출하기 위함
+//          --> 실패, 채점 80%정도 되었을때 실패했다고 뜸. 어느 테스트케이스를 충족 못 시키는지 모르겠음
+// 3차 시도 : 결국 HashMap<부모, [자식들]>로 풀지 않고 배열 사용
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Tree1068 {
-    static HashMap<Integer, ArrayList<Integer>> hashMap;
+    static int n;
+    static int[] tree;
+    static int numLeafNode = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        tree = new int[n];
+
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        hashMap = new HashMap<>();
-
         int root = 0;
-        for(int node=0; node<n; node++) {
-            int parent = Integer.parseInt(st.nextToken());
-            if (parent == -1) {
-                root = node;
-            }
-            if (hashMap.get(parent) == null) {      // 자식노드가 없는 부모노드라면, 새로운 childArray리스트 추가
-                ArrayList<Integer> childArrayList = new ArrayList<>();
-                childArrayList.add(node);
-                hashMap.put(parent, childArrayList);
-            } else {        // 자식노드가 이미 있는 parent라면, 추가된 자식을 기존 childArrayList에 업데이트
-                ArrayList<Integer> updateChildList = hashMap.get(parent);
-                updateChildList.add(node);
-                hashMap.replace(parent, updateChildList);
+
+        for (int i = 0; i < n; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+            if (tree[i] == -1) {
+                root = i;
             }
         }
 
+        // 삭제
         int removeNodeAt = Integer.parseInt(br.readLine());
-        hashMap.remove(removeNodeAt);       // 삭제할 노드를 키값으로 가지는 values, 즉 자식노드들 없애기
+        removeRecursively(removeNodeAt);
 
-        int count = 0;
-        count += searchLeafNode(root);  // 루트에서 시작!
+        for(int i=0; i<n; i++) {
+            if (tree[i] != -1){
+                searchLeafNode(i);
+            }
+        }
+        System.out.println(numLeafNode);
+}
 
-        // 삭제할 노드가 리프노드로서 남아있기 때문에
-        System.out.println(count - 1);
-
+    public static void removeRecursively(int parent) {
+        tree[parent] = -1;
+        for(int i=0; i<n; i++) {
+            if(tree[i] == parent) {
+                tree[i] = -1;
+                removeRecursively(i);
+            }
+        }
     }
 
-    public static int searchLeafNode(int parent) {
-        int ret = 0;
-
-        // 아니라면 또 호출
-        ArrayList<Integer> childList = new ArrayList<>();
-        childList = hashMap.get(parent);
-
-        // 자식노드가 있다면
-        if( childList != null ) {
-            for(int each : childList) {
-                ret += searchLeafNode(each);
+    public static void searchLeafNode(int parent) {
+        for(int i=0; i<n; i++) {
+            if( tree[i] == parent) {
+                return;
             }
-            return ret;
-        } else {
-            // 마지막 leaf node라면
-            return ret+1;
         }
+        numLeafNode++;
     }
 }
