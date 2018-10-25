bba1556d8ca5093410f83c7bee619801ad3ae15a
suyeon <synature14@gmail.com>
2018-10-26 00:29:52 +0900

    #1753 - 의미없는 수정..

--- src/com/study181030/ShortestPath1753.java
+++ src/com/study181030/ShortestPath1753.java
@@ -1,5 +1,4 @@
 package com.study181030;
-
 // 다익스트라 알고리즘 with PriorityQueue
 // 1차시도 : 배열 [nVertex+1][nVertex+1]로 풀어서 메모리초과
 // 2차시도 : 이중 ArrayList 활용
@@ -43,10 +42,10 @@ public class ShortestPath1753 {
 
         for(int i=0; i<=nVertex; i++) {
             distance[i] = INF;          // 초기화
-            graph[i] = new ArrayList<Pair>();
+            graph[i] = new ArrayList<>();
         }
 
-        for(int i=0; i<nEdge; i++) {
+        for(int i=1; i<=nEdge; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
@@ -57,30 +56,32 @@ public class ShortestPath1753 {
 
         distance[start] = 0;     // 시작점 ~ 자기 자신까지의 경로는 0개
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(start);
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(start, 0));
 
         while(!pq.isEmpty()) {
-            int sourceIndex = pq.poll();
+            Pair currentNode = pq.poll();
+            int currentIndex = currentNode.source;
 
-            for(int i=0; i<graph[sourceIndex].size() ; i++) {
+            if(distance[currentIndex] < currentNode.weight ) continue;       // start~source까지의 가중치 < 방금 찾은 가중치 : 무시하면 되는 케이스
+
+            for(int i=0; i<graph[currentIndex].size(); i++) {
                 // sourceIndex -> i로 가는 경로가 있다면
-                int destIndex = graph[sourceIndex].get(i).source;
-                int nextDistance = graph[sourceIndex].get(i).weight + distance[sourceIndex];    // 정점 i까지 도착하는 비용구하고,
+                Pair nextNode = graph[currentIndex].get(i);
 
                 // 기존에 저장해 놓은 것 보다 더 짧은 경로 발견하면
-                if(distance[destIndex] > nextDistance) {
-                    distance[destIndex] = nextDistance;
-                    pq.add(destIndex);
+                if(distance[nextNode.source] > nextNode.weight + distance[currentIndex]) {
+                    distance[nextNode.source] = distance[currentIndex] + nextNode.weight;
+                    pq.add(new Pair(nextNode.source, distance[nextNode.source]));      // destination
                 }
             }
         }
 
-        for(int i=1; i<=nVertex; i++) {
-            if (distance[i] != INF) {   // 초기화한 값이 그대로 남아있다면 경로를 못찾음것임!
-                System.out.println(distance[i]);
-            } else {
+        for(int i=1; i<distance.length; i++) {
+            if (distance[i] == INF) {   // 초기화한 값이 그대로 남아있다면 경로를 못찾음것임!
                 System.out.println("INF");
+            } else {
+                System.out.println(distance[i]);
             }
         }
 
