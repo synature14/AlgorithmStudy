43736938de357f3c69742329d39bbd4825e466de
suyeon <synature14@gmail.com>
2018-10-25 18:28:02 +0900

    #1753 주어진 테스트케이스 다 맞췄는데 자꾸 85%채점에서 실패함

--- src/com/study181030/ShortestPath1753.java
+++ src/com/study181030/ShortestPath1753.java
@@ -1,10 +1,13 @@
 package com.study181030;
 
 // 다익스트라 알고리즘 with PriorityQueue
+// 1차시도 : 배열 [nVertex+1][nVertex+1]로 풀어서 메모리초과
+// 2차시도 : 이중 ArrayList 활용
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -12,7 +15,7 @@ public class ShortestPath1753 {
     static int nVertex;
     static int nEdge;
     static int start;
-
+    static int INF = 100;
     public static class Pair implements Comparable<Pair> {
         int source;
         int weight;
@@ -24,7 +27,7 @@ public class ShortestPath1753 {
 
         @Override
         public int compareTo(Pair o) {      // 우선순위 큐를 사용하기 위해선 Comparable 인터페이스를 따라야함. 안그러면 NullPointerException 발생
-            return 0;
+            return this.weight < o.weight ? -1 : 1;
         }
     }
 
@@ -36,50 +39,48 @@ public class ShortestPath1753 {
         nEdge = Integer.parseInt(st.nextToken());
         start = Integer.parseInt(br.readLine());
         int[] distance = new int[nVertex+1];    // i까지의 최단경로
-        Pair[][] graph = new Pair[nVertex+1][nVertex+1];  // row: 시작점, column: 인점한 정점 저장
+        ArrayList<Pair>[] graph = new ArrayList[nVertex+1];
 
-        for(int i=1; i<=nVertex; i++) {
+        for(int i=0; i<=nVertex; i++) {
+            distance[i] = INF;          // 초기화
+            graph[i] = new ArrayList<Pair>();
+        }
+
+        for(int i=0; i<nEdge; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-
-            graph[from][to] = new Pair(to, cost);   // ex) graph[2][3] = Pair(3, 4) : 5->1로가는 간선의 가중치는 1
-            distance[i] = 100;
+            graph[from].add(new Pair(to, cost));
         }
+        br.close();
 
         distance[start] = 0;     // 시작점 ~ 자기 자신까지의 경로는 0개
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(start, 0));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(start);
 
         while(!pq.isEmpty()) {
-            int sourceIndex = pq.peek().source;
-            int weight = pq.peek().weight;
-            pq.poll();
+            int sourceIndex = pq.poll();
 
-            if(distance[sourceIndex] < weight ) continue;       // start~source까지의 가중치 < 방금 찾은 가중치 : 무시하면 되는 케이스
+            for(int i=0; i<graph[sourceIndex].size() ; i++) {
+                // sourceIndex -> i로 가는 경로가 있다면
+                int destIndex = graph[sourceIndex].get(i).source;
+                int nextDistance = graph[sourceIndex].get(i).weight + distance[sourceIndex];    // 정점 i까지 도착하는 비용구하고,
 
-            for(int i=1; i<=nVertex; i++) {
-                if(graph[sourceIndex][i] != null) {      // sourceIndex -> i로 가는 경로가 있다면
-                    int destIndex = graph[sourceIndex][i].source;   // 사실상 'i' equals to 'destIndex' 임!
-                    int nextDistance = weight + graph[sourceIndex][i].weight;    // 정점 i까지 도착하는 비용구하고,
-
-                    // 기존에 저장해 놓은 것 보다 더 짧은 경로 발견하면
-                    if(distance[destIndex] > nextDistance) {
-                        distance[destIndex] = nextDistance;
-                        pq.add(new Pair(destIndex, nextDistance));      // destination
-                    }
+                // 기존에 저장해 놓은 것 보다 더 짧은 경로 발견하면
+                if(distance[destIndex] > nextDistance) {
+                    distance[destIndex] = nextDistance;
+                    pq.add(destIndex);
                 }
             }
         }
 
-        System.out.println();
-        for(int i=1; i<distance.length; i++) {
-            if (distance[i] == 100) {   // 초기화한 값이 그대로 남아있다면 경로를 못찾음것임!
-                System.out.println("INF");
-            } else {
+        for(int i=1; i<=nVertex; i++) {
+            if (distance[i] != INF) {   // 초기화한 값이 그대로 남아있다면 경로를 못찾음것임!
                 System.out.println(distance[i]);
+            } else {
+                System.out.println("INF");
             }
         }
 
