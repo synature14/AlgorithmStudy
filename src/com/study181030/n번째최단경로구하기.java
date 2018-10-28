55d78057c498bf666680b9be76435475c2d8c58d
suyeon <synature14@gmail.com>
2018-10-28 17:57:07 +0900

    k번째로 최단인 경로찾기 추가

--- src/com/study181030/n번째최단경로구하기.java
+++ src/com/study181030/n번째최단경로구하기.java
@@ -1,17 +1,15 @@
 package com.study181030;
-
-// 1차시도 : 그래프를 2차원 배열로 표현!, visited표시, DFS로 탐색
-// 2차시도 : 최단경로 구한 후, 최단경로만 지워나간다
+// k번째 최단경로!!
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class AlmostShortestPath5719 {
+public class n번째최단경로구하기 {
     static boolean[][] visited;
     static int n;
-    static int[] distance;
+    static PriorityQueue<Integer>[] distance;
     static int destination;
     static ArrayList<Path>[] savingEdge;
     static int start;
@@ -52,9 +50,13 @@ public class AlmostShortestPath5719 {
             st = new StringTokenizer(sd, " ");
             start = Integer.parseInt(st.nextToken());
             destination = Integer.parseInt(st.nextToken());
-            distance = new int[n+1];
+            distance = new PriorityQueue[n+1];
             graph = new ArrayList[m+1];
             visited = new boolean[n][n];
+
+            for(int i=0; i<=n; i++) {
+                distance[i] = new PriorityQueue<>();
+            }
 
             for(int i=0; i<=m; i++) {
                 graph[i] = new ArrayList<>();
@@ -70,12 +72,13 @@ public class AlmostShortestPath5719 {
                 graph[from].add(new Path(to, weight));
             }
 
-            dijkstra();
-            removeEdge();
-            dijkstra();
+            dijkstra(2);
 
-            if (distance[destination] != INF) {
-                answerQueue.add(distance[destination]);
+
+            if (distance[destination].size() == 2) {      // 2번째 최단경로의 크기가 2라면
+                // 첫번째로 짧은 경로는 버리고
+                distance[destination].poll();
+                answerQueue.add(distance[destination].poll());  // 두번째 최단경로만 넣는다
             } else {
                 answerQueue.add(-1);
             }
@@ -89,9 +92,8 @@ public class AlmostShortestPath5719 {
 
     }
 
-    public static void dijkstra() {        // k번째 최단경로
-        distance[start] = 0;
-        Arrays.fill(distance, INF);
+    public static void dijkstra(int k) {        // k번째 최단경로
+        distance[start].add(0);
 
         PriorityQueue<Path> pq = new PriorityQueue<>();
         pq.add(new Path(start, 0));
@@ -105,35 +107,17 @@ public class AlmostShortestPath5719 {
                 int nextIndex = graph[currentIndex].get(i).source;
                 int nextWeight = graph[currentIndex].get(i).weight;
 
-                if (distance[nextIndex] > nextWeight + currentWeight
-                        && visited[currentIndex][nextIndex] == false) {
-                    distance[nextIndex] = nextWeight + currentWeight;
+                if(distance[nextIndex].size() < k) {        // nextIndex의 우선순위 큐 크기가 찾으려는 k개까지만 존재하면 됨
+                    distance[nextIndex].add(currentWeight + nextWeight);
+                    pq.add(new Path(nextIndex, currentWeight + nextWeight));
+
+                } else if (distance[nextIndex].peek() > nextWeight + currentWeight) {     // 저장한 경로 개수가 k개 이상일경우, 더 짧은 거리 찾았다면, 갱신
+                    distance[nextIndex].poll();
+                    distance[nextIndex].add(nextWeight + currentWeight);
                     pq.add(new Path(nextIndex, currentWeight + nextWeight));
                 }
             }
         }
-    }
-
-    public static void removeEdge() {
-        PriorityQueue<Path> pq = new PriorityQueue<>();
-        pq.add(new Path(destination, 0));
-
-        while(!pq.isEmpty()) {
-            Path current = pq.poll();
-            int currentIndex = current.source;
-            int currentWeight = current.weight;
-
-            for(Path each : graph[currentIndex]) {
-                int nextIndex = each.source;
-
-                if(distance[nextIndex] == distance[currentIndex] + each.weight) {
-                    visited[currentIndex][nextIndex] = true;
-                    pq.add(new Path(nextIndex, currentWeight + each.weight));
-                }
-            }
-        }
-
-
     }
 
 
